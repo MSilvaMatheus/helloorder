341c564f409ca509a387a238eab8136de2797353
Matheus Maia <matheusmaiadasilva93@gmail.com>
2020-08-05 00:30:09 -0300

    first commit

---
+++ BettyBeer/Controllers/BettyBeerController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace BettyBeer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BettyBeerController : ControllerBase
+    {        
+        private readonly ILogger<BettyBeerController> _logger;
+
+        public BettyBeerController(ILogger<BettyBeerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            Url generator = new Url("https://www.globo.com/");
+            string payload = generator.ToString();
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            var qrCodeAsBitmap = qrCode.GetGraphic(20);
+
+            var byteArray = BitmapToBytes(qrCodeAsBitmap);
+            return Ok(Convert.ToBase64String(byteArray));
+        }
+
+        private byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+
+    public static class ImageExtensions
+    {
+        public static byte[] ToByteArray(this Image image, ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                return ms.ToArray();
+            }
+        }
+    } 
+}
